fff44b9f514a62aa08fa9475a86c54bfa8c37b7e
aliafzalh <aliafzalh@gmail.com>
2025-07-20 21:39:16 -0400

    4th commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 // const express = require('express');
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = 8080;
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello (from server)');
+  res.json('Hello (from server cors)');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    try {
+      async function getData() {
+      const respose = await fetch('http://localhost:8080')
+      const data = await respose.json()
+      console.log(data)
+    }
+    } catch (error) {
+      console.log(error)
+    }
+
+    
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    Hello (from frontend)
+
     </>
   )
 }
