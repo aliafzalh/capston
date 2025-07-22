82e713d6399e353a4fba1e764ca58fd443c22278
aliafzalh <aliafzalh@gmail.com>
2025-07-22 00:50:55 -0400

    6th commit

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,22 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import connectDb from './db.js';
+import Todo from './models/todo.js';
+
 const app = express()
 const port = process.env.PORT
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json('Hello (from server Middleware_cors)');
+app.get('/todos', async (req, res) => {
+try {
+   const todos = await Todo.find({})
+   res.status(200).json(todos)
+   res.json(todos);
+} catch (error) {
+  console.log(error)
+  res.status(400).json(error)
+}
   
 });
 
